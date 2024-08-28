0447853a6bbff868d4e73b5dbd364b27575906da
Eqilia Luminé <eqilia@national.shitposting.agency>
2024-08-28 16:29:03 +0200

    add money module

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
 'use strict';
 
+const XorShift = require('xorshift');
+
+const money = require('./money.js');
+
 console.log("You aren't here to cheat, right?");
+
+updateMoneyDisplay();
---
+++ src/js/money.js
@@ -0,0 +1,15 @@
+function getMoney() {
+	const encodedMoney = localStorage.getItem('money');
+	const decodedMoney = btoa(encodedMoney);
+	const parsedMoney = parseInt(decodedMoney);
+
+	return parsedMoney;
+}
+
+function setMoney(amount) {
+	return localStorage.setItem('money', amount.toString());
+}
+
+function updateMoneyDisplay() {
+	document.getElementById('money').innerText = `money $${getMoney()}`
+}
